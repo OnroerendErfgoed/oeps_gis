2d7a7a372920d4f0a8daa65a78604c679313d647
Maarten Vermeyen <vermeyen.m@gmail.com>
2014-09-05 14:42:52 +0200

    Update setup.py to work with cxFreeze

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
 
-setup(console=['export.py'])
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "oeps_gis",
+        version = "0.2",
+        description = "OEPS export",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("export.py", base=base)])
+
 
 
