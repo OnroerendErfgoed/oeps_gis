7a94e1523fc036f917090eed80bf94f83f74ebcd
Maarten Vermeyen <vermeyen.m@gmail.com>
2014-09-05 15:05:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {
+    "packages": ["os", "osgeo"], 
+    "excludes": ["tkinter"],
+    "include_msvcr": True
+    }
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
